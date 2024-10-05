4381ccb46fe90e179254c416a1aec9a2782382d4
thefelipe001 <elfelipe_lugo@hotmail.com>
2024-10-05 13:49:13 -0400

    Agregando Mejora en API

--- PeopleCatalogSolution/PeopleCatalog.Application/DTOs/PersonDto.cs
+++ PeopleCatalogSolution/PeopleCatalog.Application/DTOs/PersonDto.cs
@@ -4,14 +4,14 @@ namespace PeopleCatalog.Application.DTOs
     public class PersonDto
     {
         public int Id { get; set; }
-        public string? FirstName { get; set; }      // Obligatorio, no nullable
-        public string? LastName { get; set; }       // Obligatorio, no nullable
-        public string? Email { get; set; }          // Obligatorio, no nullable
-        public DateTime DateOfBirth { get; set; }  // Obligatorio, no nullable
-        public string? PhoneNumber { get; set; }   // Nullable, puede ser nulo
-        public string? Street { get; set; }         // Obligatorio, no nullable
-        public string? City { get; set; }           // Obligatorio, no nullable
-        public string? PostalCode { get; set; }    // Nullable, puede ser nulo
+        public string? FirstName { get; set; }     
+        public string? LastName { get; set; }       
+        public string? Email { get; set; }          
+        public DateTime DateOfBirth { get; set; }  
+        public string? PhoneNumber { get; set; }   
+        public string? Street { get; set; }         
+        public string? City { get; set; }           
+        public string? PostalCode { get; set; }    
     }
 
 
